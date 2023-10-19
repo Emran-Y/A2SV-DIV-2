a1b90a36488bace9698e42ff462eff97dc4d2eda
EMRAN YONAS YIMER <139449306+Emran-Y@users.noreply.github.com>
2023-10-19 15:47:33 -0500

    Create checkIfTheArrayIsSorted.py

---
+++ checkIfTheArrayIsSorted.py
@@ -0,0 +1,8 @@
+class Solution:
+    def arraySortedOrNot(self, arr, n):
+        # code here
+        for i in range(len(arr) - 1):
+            if(arr[i] > arr[i+1]):
+                return 0
+            
+        return 1
