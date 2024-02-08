8a2f19eb3e88fe13827604f3f27d000511dbdaf4
EMRAN <139449306+Emran-Y@users.noreply.github.com>
2024-02-08 10:01:18 +0300

    Add solution for Find the Middle Index in Array

--- leetcode/leetcode/find-the-middle-index-in-array.py
+++ leetcode/leetcode/find-the-middle-index-in-array.py
@@ -1,6 +1,6 @@
 class Solution:
-    def findMiddleIndex(self, nums: List[int]) -> int:\
-    
+    def findMiddleIndex(self, nums: List[int]) -> int:
+
         prefix_sum = [0]
         for i in nums:
             prefix_sum.append(prefix_sum[-1] + i)
