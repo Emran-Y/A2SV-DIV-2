54dcffbf18ec2623343664cc721c53c62b132e17
EMRAN <139449306+Emran-Y@users.noreply.github.com>
2024-05-26 09:41:27 +0300

    Created 'Maximum Performance of a Team' by A2SV Hub

---
+++ 26-May-2024/Maximum Performance of a Team 159606.py
@@ -0,0 +1,15 @@
+# Problem: Maximum Performance of a Team - https://leetcode.com/problems/maximum-performance-of-a-team/
+
+n, s = map(int, input().split())
+if(n==1 and s==0):
+    print('0 0')
+elif(9*n < s or s == 0):
+    print('-1 -1')
+else:
+    x1 = 10**(n-1)
+    for i in range(s-1):
+        x1 += 10**(i//9)
+    x2 = 0
+    for i in range(s):
+        x2 += 10**(n-1-i//9)
+    print(x1, x2)
